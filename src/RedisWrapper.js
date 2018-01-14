b7d5195ac7eb72f14b1787aa96f4ba45ed817db0
Rafał Ruciński <fatfisz@codility.com>
2018-01-14 16:14:41 +0100

    Fix scriptLoad

--- src/RedisWrapper.js
+++ src/RedisWrapper.js
@@ -7,7 +7,7 @@ const redis = require('redis');
 
 type BlpopResult = [string, string] | null;
 
-class RedisWrapper {
+class BaseRedisWrapper {
   client: redis.RedisClient;
 
   constructor(options: mixed) {
@@ -17,6 +17,13 @@ class RedisWrapper {
   quit() {
     this.client.quit();
   }
+}
+
+class RedisWrapper extends BaseRedisWrapper {
+  wrap(method: Function): Function {
+    const promisified = util.promisify(method);
+    return promisified.bind(this.client);
+  }
 
   registerScript(script: string) {
     const sha = this.scriptLoad(script);
@@ -24,26 +31,23 @@ class RedisWrapper {
       this.evalsha(await sha, keys, args);
   }
 
-  wrap(method: Function): Function {
-    const promisified = util.promisify(method);
-    return promisified.bind(this.client);
-  }
+  blpop: (keys: Array<string>, timeout: number) => Promise<BlpopResult> = this.wrap(
+    (keys, timeout, callback) => this.client.blpop(...keys, timeout, callback),
+  );
+
+  evalsha: (sha: string, keys: Array<string>, args: Array<mixed>) => Promise<void> = this.wrap(
+    (sha, keys, args, callback) =>
+      this.client.evalsha(sha, keys.length, ...keys, ...args, callback),
+  );
+
+  scriptLoad: (script: string) => Promise<string> = this.wrap((script, callback) =>
+    this.client.script('load', script, callback),
+  );
 
   rpushex = this.registerScript(`
     redis.call('rpush', KEYS[1], ARGV[1]);
     redis.call('expire', KEYS[1], ARGV[2]);
   `);
-
-  blpop: (keys: Array<string>, timeout: number) => Promise<BlpopResult> = this.wrap(
-    (keys, timeout, callback) => this.client.blpop(...keys, timeout, callback),
-  );
-  evalsha: (sha: string, keys: Array<string>, args: Array<mixed>) => Promise<void> = this.wrap(
-    (sha, keys, args, callback) =>
-      this.client.evalsha(sha, keys.length, ...keys, ...args, callback),
-  );
-  scriptLoad: (script: string) => Promise<string> = this.wrap((script, callback) =>
-    this.client.script('load', callback),
-  );
 }
 
 module.exports = RedisWrapper;
